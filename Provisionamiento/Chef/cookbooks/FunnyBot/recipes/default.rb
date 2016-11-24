0e6f8de3a7394ef0d1aac23d0d86d89654737514
pedrogazquez <pedrogazqueznavarrete@gmail.com>
2016-11-24 13:53:21 +0100

    Añadiendo chef

---
+++ Provisionamiento/Chef/cookbooks/FunnyBot/recipes/default.rb
@@ -0,0 +1,8 @@
+apt_update 'all platforms'
+
+package 'mongodb'
+package 'python-setuptools'
+package 'python-dev'
+package 'python-pip'
+
+easy_install_package 'python-telegram-bot'
---
+++ Provisionamiento/Chef/solo.rb
@@ -0,0 +1,3 @@
+file_cache_path "/home/ubuntu/FunnyBot/Provisionamiento/Chef/chef"
+cookbook_path "/home/ubuntu/FunnyBot/Provisionamiento/Chef/chef/cookbooks"
+json_attribs "/home/ubuntu/FunnyBot/Provisionamiento/Chef/chef/node.json"
